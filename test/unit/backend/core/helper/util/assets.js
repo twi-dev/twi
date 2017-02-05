633a6cd09b0c77142b2a5e0aba8e6a3aac6e2829
Nick K <nick.kruchinin@gmail.com>
2017-02-05 03:14:00 +0300

    One more test -_-

---
+++ test/unit/backend/core/helper/util/assets.js
@@ -0,0 +1,66 @@
+import test from "ava"
+import pq from "proxyquire"
+import isFunction from "lodash/isFunction"
+
+const ROOT = process.cwd()
+
+const mockAssets = {
+  main: {
+    js: "magic.js"
+  }
+}
+
+const assets = pq(`${ROOT}/core/helper/util/assets`, {
+  ["promise-fs"]: {
+    readFile: () => JSON.stringify(mockAssets)
+  }
+}).default
+
+test("Should be a function", async t => {
+  t.plan(1)
+
+  t.true(isFunction(assets))
+})
+
+test("Should return a Promise", async t => {
+  t.plan(1)
+
+  t.true(assets() instanceof Promise)
+})
+
+test("Should resolve a function", async t => {
+  t.plan(1)
+
+  t.true(isFunction(await assets()))
+})
+
+test("Should return a required assets type", async t => {
+  t.plan(1)
+
+  const getAssets = await assets()
+
+  t.deepEqual(getAssets("js"), ["magic.js"])
+})
+
+test(
+  "Should throw a TypeError when getAssets has been called without type",
+  async t => {
+    t.plan(1)
+
+    const getAssets = await assets()
+
+    const tryCatch = () => getAssets()
+
+    t.throws(tryCatch, "Asset type cannot be empty.")
+  }
+)
+
+test("Should throw a TypeError when asset type is not a string", async t => {
+  t.plan(1)
+
+  const getAssets = await assets()
+
+  const tryCatch = () => getAssets(451)
+
+  t.throws(tryCatch, "Asset type should be a string.")
+})
