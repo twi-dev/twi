0d99a0641abca98b3d6f3c322d308326e16ad3e7
Nick K <nick.kruchinin@gmail.com>
2017-04-18 00:47:58 +0300

    Refactored next.config.js

--- next.config.js
+++ next.config.js
@@ -1,47 +1,21 @@
 const {join} = require("path")
 
 const rd = require("require-dir")
-const UglifyJsPlugin = require("webpack").optimize.UglifyJsPlugin
-const BabiliWebpackPlugin = require("babili-webpack-plugin")
 
 const objectIterator = require(
   join(__dirname, "system/helper/iterator/sync/objectIterator")
 )
 
-function getRules(options) {
-  const res = []
+function loadConfig() {
+  const res = {}
 
-  const rules = rd(join(__dirname, "setup/frontend/rule"))
+  const parts = rd(join(__dirname, "setup/frontend/config"))
 
-  for (const val of objectIterator.default(rules)) {
-    res.push(val.default(options))
+  for (const [name, part] of objectIterator.default.entries(parts)) {
+    res[name] = part.default || part[name] || part
   }
 
   return res
 }
 
-function webpack(cfg, {dev}) {
-  const rules = getRules({isDev: dev})
-
-  cfg.plugins = cfg.plugins.map(
-    p => p instanceof UglifyJsPlugin ? new BabiliWebpackPlugin() : p
-  )
-
-  cfg.resolveLoader.alias = {
-    "transform-file-loader": (
-      `${__dirname}/setup/frontend/loader/transform-file-loader`
-    )
-  }
-
-  cfg.resolve.extensions = [".js", ".jsx", ".json"]
-
-  cfg.resolve.modules.push(join(__dirname, "frontend"))
-
-  cfg.module.rules.push(...rules)
-
-  return cfg
-}
-
-module.exports = {
-  webpack
-}
+module.exports = loadConfig()
---
+++ src/setup/frontend/config/webpack.js
@@ -0,0 +1,44 @@
+import {resolve} from "path"
+
+import {optimize} from "webpack"
+
+import rd from "require-dir"
+import BabiliWebpackPlugin from "babili-webpack-plugin"
+
+import objectIterator from "system/helper/iterator/sync/objectIterator"
+
+function getRules(options) {
+  const res = []
+
+  const rules = rd(resolve(__dirname, "..", "rule"))
+
+  for (const val of objectIterator(rules)) {
+    res.push(val.default(options))
+  }
+
+  return res
+}
+
+function webpack(cfg, {dev}) {
+  const rules = getRules({isDev: dev})
+
+  cfg.plugins = cfg.plugins.map(
+    p => p instanceof optimize.UglifyJsPlugin ? new BabiliWebpackPlugin() : p
+  )
+
+  cfg.resolveLoader.alias = {
+    "transform-file-loader": (
+      `${__dirname}/setup/frontend/loader/transform-file-loader`
+    )
+  }
+
+  cfg.resolve.extensions = [".js", ".jsx", ".json"]
+
+  cfg.resolve.modules.push(resolve(__dirname, "..", "..", "frontend"))
+
+  cfg.module.rules.push(...rules)
+
+  return cfg
+}
+
+export default webpack
