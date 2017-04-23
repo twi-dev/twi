77080960d97dc457cf70ff7590c0c43357c5ff4b
Nick K <nick.kruchinin@gmail.com>
2017-04-23 18:53:21 +0300

    Tiny fix in a test

--- test/unit/system/helper/typechecker/isListOf.js
+++ test/unit/system/helper/typechecker/isListOf.js
@@ -5,7 +5,7 @@ import isListOf from "system/helper/typechecker/isListOf"
 test(
   "Should return true when each element of given list is a valid type",
   t => {
-    const list = ["earch pony", "unicorn", "pegasus", "alicorn"]
+    const list = ["earth pony", "unicorn", "pegasus", "alicorn"]
 
     const predicate = val => typeof val === "string"
 
