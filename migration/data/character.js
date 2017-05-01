5bfceca080c02eef16971059cf667599e2d681f9
Nick K <nick.kruchinin@gmail.com>
2017-05-01 21:05:28 +0300

    Add readConfig.

--- migration/data/character.js
+++ migration/data/character.js
@@ -1,5 +1,3 @@
-"use strict"
-
 module.exports = [
   {
     pic: "twilight-sparkle.png"
--- src/setup/frontend/config/webpack.js
+++ src/setup/frontend/config/webpack.js
@@ -7,6 +7,8 @@ import BabiliWebpackPlugin from "babili-webpack-plugin"
 
 import objectIterator from "system/helper/iterator/sync/objectIterator"
 
+const UglifyJsPlugin = optimize.UglifyJsPlugin
+
 function getRules(options) {
   const res = []
 
@@ -19,11 +21,15 @@ function getRules(options) {
   return res.reverse()
 }
 
+const isUglifyJsPlugin = val => (
+  val instanceof UglifyJsPlugin || val.constructor.name === "UglifyJsPlugin"
+)
+
 function webpack(cfg, {dev}) {
   const rules = getRules({isDev: dev})
 
   cfg.plugins = cfg.plugins.map(
-    p => p instanceof optimize.UglifyJsPlugin ? new BabiliWebpackPlugin() : p
+    p => isUglifyJsPlugin(p) ? new BabiliWebpackPlugin() : p
   )
 
   cfg.resolveLoader.alias = {
