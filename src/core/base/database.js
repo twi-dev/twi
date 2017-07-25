d1cae0bc1c5bad7ad7df5c3e4d18247bf4e9baf9
Nick K <nick.kruchinin@gmail.com>
2017-07-26 02:06:53 +0300

    Update dependencies.

--- src/core/base/database.js
+++ src/core/base/database.js
@@ -6,27 +6,15 @@ import invariant from "core/helper/util/invariant"
 mongoose.Promise = Promise
 
 function getConnectionSctring({host, port, name}) {
+  invariant(host == null, "Host is required for a connection.")
+
+  invariant(!isString(host), TypeError, "Host should be passed as a string.")
+
+  invariant(name == null, "Database is required for a connection.")
+
+  invariant(!isString(name), TypeError, "Database name should be a string.")
+
   let connectionSctring = "mongodb://"
-
-  invariant(
-    host == null, TypeError,
-    "Host is required parameter for database connection."
-  )
-
-  invariant(
-    isString(host) === false, TypeError,
-    "Host should be passed as a string."
-  )
-
-  invariant(
-    name == null, TypeError,
-    "Database is required for a connection."
-  )
-
-  invariant(
-    isString(name) === false, TypeError,
-    "Database name should be a string."
-  )
 
   connectionSctring += host
 
