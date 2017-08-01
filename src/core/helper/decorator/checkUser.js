2b1c5a0c6f3dd1a2dd78cd86aa0111ff16e1554a
Nick K <nick.kruchinin@gmail.com>
2017-08-02 01:09:45 +0300

    Add checkUser decorator helper.

---
+++ src/core/helper/decorator/checkUser.js
@@ -0,0 +1,40 @@
+import invariant from "@octetstream/invariant"
+import isFunction from "lodash/isFunction"
+import isString from "lodash/isString"
+
+import Forbidden from "core/error/http/Forbidden"
+
+const DEFAULT_MESSAGE = "Access denied."
+
+const checkUserDecorator = message => resolver => async function(
+  parent, args, ctx, ...others
+) {
+  message || (message = DEFAULT_MESSAGE)
+
+  invariant(!ctx.state.user, Forbidden, message)
+
+  return await resolver(parent, args, ctx, ...others)
+}
+
+/**
+ * Check if user is authenticated.
+ *
+ * @param string|function
+ *
+ * @return AsyncFunction|function
+ */
+function checkUser(messageOrTarget) {
+  if (messageOrTarget && isString(messageOrTarget)) {
+    return checkUser(messageOrTarget)
+  }
+
+  invariant(
+    !isFunction(messageOrTarget), TypeError,
+    "Target function is required."
+  )
+
+  return checkUserDecorator()(messageOrTarget)
+}
+
+export default checkUser
+export {checkUserDecorator}
--- src/graphql/resolve/mutation/story/addStory.js
+++ src/graphql/resolve/mutation/story/addStory.js
@@ -1,7 +1,9 @@
 import Story from "database/model/Story"
 
+import checkUser from "core/helper/decorator/checkUser"
+
 async function addStory(_, {story}, ctx) {
   return await Story.createOne(ctx.state.user.id, story, ctx)
 }
 
-export default addStory
+export default checkUser(addStory)
