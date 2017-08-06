ef68b02c5b1d5b22867eba0133c752343eddcb5d
Nick K <nick.kruchinin@gmail.com>
2017-08-07 01:50:29 +0300

    Fix for TUserRoleInput enum type.

--- src/graphql/input/user/TUserRoleInput.js
+++ src/graphql/input/user/TUserRoleInput.js
@@ -3,7 +3,7 @@ import Enum from "parasprite/Enum"
 const TUserRole = Enum("UserRole")
   .value("ADMIN", "admin")
   .value("MODERATOR", "moderator")
-  .value("REGULAR", "regular")
+  .value("USER", "user")
 .end()
 
 export default TUserRole
