4d976b17f01c06bac4a4e44e6081488e1789e1b5
Nick K <nick.kruchinin@gmail.com>
2017-10-09 01:54:57 +0300

    Add Story middlewares.

--- src/database/middleware/story.js
+++ src/database/middleware/story.js
@@ -1,14 +1,34 @@
-// import invariant from "@octetstream/invariant"
+import invariant from "@octetstream/invariant"
+import isEmpty from "lodash/isEmpty"
 
-// import {before} from "core/database/middleware"
+import {before, after} from "core/database/middleware"
 
-// async function beforeSave(story) {
-//   invariant(
-//     story.translation.flag && !story.translation.author,
-//     "Author's information required for translations"
-//   )
-// }
+import Chapter from "database/model/Chapter"
 
-// module.exports = {
-//   beforeSave: before.save(beforeSave)
-// }
+async function afterValidate({translation}) {
+  if (translation.flag === false) {
+    return true
+  }
+
+  const author = translation.author
+
+  invariant(
+    isEmpty(author), "Author's information required for translations."
+  )
+
+  invariant(!author.name, "Author's name is required.")
+
+  // TODO: Also, add a link validation
+  invariant(!author.profile, "Required a link to the author's profile.")
+
+  invariant(!translation.original, "Required a link to the original story.")
+}
+
+async function beforeRemove(story) {
+  await Chapter.removeManyById(story.chapters)
+}
+
+module.exports = {
+  beforeRemove: before.remove(beforeRemove),
+  afterValidate: after.validate(afterValidate)
+}
