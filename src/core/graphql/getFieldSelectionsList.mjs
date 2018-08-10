6456022ddec6b09ad3890f46c1f363660b2dc03f
Nick K <nick.kruchinin@gmail.com>
2018-08-11 01:51:56 +0300

    Fix graphql getFieldSelectionsList helper.

--- src/core/graphql/getFieldSelectionsList.mjs
+++ src/core/graphql/getFieldSelectionsList.mjs
@@ -1,15 +1,9 @@
 function getFieldSelectionsList(node) {
-  if (!node || node.fieldName || !node.fieldNodes) {
-    return []
-  }
-
   const name = node.fieldName
 
   const field = node.fieldNodes.find(curr => curr.name.value === name)
 
-  return field.selectionSet.selections
-    .filter(curr => curr.selectionSet == null)
-    .map(curr => curr.name.value)
+  return field.selectionSet.selections.map(curr => curr.name.value)
 }
 
 export default getFieldSelectionsList
