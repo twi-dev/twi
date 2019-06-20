b17cb295377d2ca0ae1a996028d03a1a3af1bca6
Nick K <nick.kruchinin@gmail.com>
2019-06-20 19:37:24 +0300

    Rewrite middlewares auto-loading.

--- src/core/base/middleware.mjs
+++ src/core/base/middleware.mjs
@@ -1,16 +1,19 @@
-import {resolve, join} from "path"
-import {readdirSync} from "fs"
+import {readFileSync} from "fs"
+import {join} from "path"
 
-const dir = resolve(__dirname, "..", "middleware")
-const list = readdirSync(dir)
+const path = join(__dirname, "..", "middleware")
+const lines = readFileSync(join(path, ".sort"), "utf-8")
+  .replace(/(#|\/\/).*$/gm, "") // Remove single line comments
+  .split(/\r?\n/) // Split file per line
+  .map(line => line.trim()) // Remove all spaces around each line
+  .filter(String) // Remove all empty lines
 
 const middlewares = []
-for (const name of list) {
-  if (/^([0-9]+)-/.test(name)) {
-    const middleware = require(join(dir, name))
 
-    middlewares.push(middleware.default || middleware)
-  }
+for (const file of lines) {
+  const middleware = require(join(path, file))
+
+  middlewares.push(middleware.default || middleware)
 }
 
 export default middlewares
