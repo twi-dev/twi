1abe8ac3202572cba1df3ca788ee801c21279e22
Nick K <nick.kruchinin@gmail.com>
2019-06-20 20:14:57 +0300

    Fix for core/middleware/static.mjs

--- src/core/middleware/static.mjs
+++ src/core/middleware/static.mjs
@@ -2,6 +2,6 @@ import {join} from "path"
 
 import kserve from "koa-static"
 
-const serve = kserve(join(__dirname, "..", "..", "..", "static"))
+const serve = kserve(join(__dirname, "..", "..", "static"))
 
 export default serve
