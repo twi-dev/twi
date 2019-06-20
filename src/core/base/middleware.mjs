2302d7ff484f73031803c71d55193389dd5c8b06
Nick K <nick.kruchinin@gmail.com>
2019-06-20 20:00:29 +0300

    Improve middlewares auto-loader

--- src/core/base/middleware.mjs
+++ src/core/base/middleware.mjs
@@ -1,17 +1,21 @@
+import {join, basename} from "path"
 import {readFileSync} from "fs"
-import {join} from "path"
 
-const path = join(__dirname, "..", "middleware")
-const lines = readFileSync(join(path, ".sort"), "utf-8")
+const getFilenames = content => content
   .replace(/(#|\/\/).*$/gm, "") // Remove single line comments
   .split(/\r?\n/) // Split file per line
   .map(line => line.trim()) // Remove all spaces around each line
   .filter(String) // Remove all empty lines
 
+const path = join(__dirname, "..", "middleware")
+
+const filenames = getFilenames(readFileSync(join(path, ".sort"), "utf-8"))
+  .map(filename => join(path, basename(filename)))
+
 const middlewares = []
 
-for (const file of lines) {
-  const middleware = require(join(path, file))
+for (const filename of filenames) {
+  const middleware = require(filename)
 
   middlewares.push(middleware.default || middleware)
 }
