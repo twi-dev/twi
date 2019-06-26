86c64597f489dbaa32eb8d48440da059529b6c63
Nick K <nick.kruchinin@gmail.com>
2019-06-26 21:50:45 +0300

    Add File.updateContent() and File#remove() methods.

--- src/db/model/File/File.mjs
+++ src/db/model/File/File.mjs
@@ -1,3 +1,5 @@
+import {unlink} from "promise-fs"
+
 import {createModel, Model} from "core/db"
 
 import calcHash from "./calcHash"
@@ -9,6 +11,17 @@ class File extends Model {
     const hash = await calcHash("sha512", path)
 
     return super.create({...fields, path, hash}, options)
+  }
+
+  async updateContent({path, ...fields}, options) {
+    const hash = await calcHash("sha512", path)
+
+    return this.update({$set: {...fields, path, hash}}, options)
+      .then(() => File.findById(this.id))
+  }
+
+  remove() {
+    return unlink(this.path).then(() => super.remove())
   }
 }
 
