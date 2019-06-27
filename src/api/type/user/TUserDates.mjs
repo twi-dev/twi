49621ba14c91f52a75318316463fafbdeb2f79d9
Nick K <nick.kruchinin@gmail.com>
2019-06-28 00:00:11 +0300

    Improve TUserDate

--- src/api/type/user/TUserDates.mjs
+++ src/api/type/user/TUserDates.mjs
@@ -2,16 +2,29 @@ import {GraphQLDateTime as TDateTime} from "graphql-iso-date"
 
 import Type from "parasprite/Type"
 
+import serialize from "core/helper/graphql/serializeDate"
+
 const TUserDates = Type("UserDates", "User activity and registration dates.")
-  .field({
+  .resolve({
     name: "registeredAt",
     type: TDateTime,
+    noArgs: true,
+    handler: serialize("registeredAt"),
     description: "User registration time mark.",
     required: true
   })
-  .field({
+  .resolve({
     name: "lastVisit",
     type: TDateTime,
+    noArgs: true,
+    handler: serialize("lastVisit"),
+    description: "User last activity time mark."
+  })
+  .resolve({
+    name: "updatedAt",
+    type: TDateTime,
+    noArgs: true,
+    handler: serialize("updatedAt"),
     description: "User last activity time mark."
   })
 .end()
