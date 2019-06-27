8dad619ee884e4a0c8ef35d6e4d186e91d1c562e
Nick K <nick.kruchinin@gmail.com>
2019-06-27 23:55:44 +0300

    Improve date serialization graphql helper.

--- src/core/helper/graphql/serializeDate.mjs
+++ src/core/helper/graphql/serializeDate.mjs
@@ -1,5 +1,9 @@
 import toDate from "date-fns/toDate"
 
-const serializeDate = name => parent => toDate(parent[name])
+const serializeDate = name => parent => {
+  const field = parent[name]
+
+  return field == null ? field : toDate(field)
+}
 
 export default serializeDate
