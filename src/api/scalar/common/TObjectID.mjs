30e1bf69a0d8376601d4cf9091b41be0684c3282
Nick K <nick.kruchinin@gmail.com>
2019-07-10 15:58:40 +0300

    Add TObjectID scalar type.

---
+++ src/api/scalar/common/TObjectID.mjs
@@ -0,0 +1,47 @@
+import {GraphQLScalarType as Scalar} from "graphql"
+import {GraphQLError} from "graphql/error"
+import {Kind} from "graphql/language"
+
+import mongoose from "mongoose"
+
+const {types: t} = mongoose.Schema
+
+const STRING = Kind.STRING
+
+function validateObjectId(ast) {
+  const {kind, value} = ast
+
+  if (kind !== STRING) {
+    throw new GraphQLError(
+      "Query error: Expected String value for TObjectID type, "
+        + `but got a type of ${kind}`,
+
+      [ast]
+    )
+  }
+
+  if (!t.ObjectId.isValid(value)) {
+    throw new GraphQLError("Invalid ObjectID value.", [ast])
+  }
+
+  return value
+}
+
+const parseValue = value => validateObjectId({
+  kind: STRING, value
+})
+
+const parseLiteral = ast => validateObjectId({...ast})
+
+const TObjectID = new Scalar({
+  name: "ObjectID",
+  description: (
+    "The user human-readable unique identifier. Allowed only "
+      + "alphabetic characters, numbers and - _ . symbols."
+  ),
+  serialize: String,
+  parseLiteral,
+  parseValue
+})
+
+export default TObjectID
