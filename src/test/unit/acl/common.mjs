cf55fecd1eb47bd44a063c941896496f188bcdfa
Nick K <nick.kruchinin@gmail.com>
2019-07-28 04:41:27 +0300

    Add acl/common.mjs file.

---
+++ src/test/unit/acl/common.mjs
@@ -0,0 +1,23 @@
+import test from "ava"
+
+import User from "db/model/User"
+import getAbilities from "acl/common"
+
+test("Allow to read only by default", t => {
+  const acl = getAbilities({})
+
+  t.true(acl.can("read", "Something"))
+  t.true(acl.cannot("manage", "Something"))
+})
+
+test("Allow super user to manage everything", t => {
+  const acl = getAbilities({role: User.roles.super})
+
+  t.true(acl.can("manage", "Something"))
+})
+
+test("Forbid banned user to manage something", t => {
+  const acl = getAbilities({status: User.statuses.super})
+
+  t.true(acl.cannot("manage", "Something"))
+})
