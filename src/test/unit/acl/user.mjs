9783ba094a01d8985541946e06828d9d1e32f286
Nick K <nick.kruchinin@gmail.com>
2019-07-28 18:56:37 +0300

    Add more tests for user ACL rules.

--- src/test/unit/acl/user.mjs
+++ src/test/unit/acl/user.mjs
@@ -1,5 +1,7 @@
 import test from "ava"
 
+import {permittedFieldsOf} from "@casl/ability/extra"
+
 import getAbilities from "acl/user"
 import UserModel from "db/model/User"
 
@@ -22,6 +24,80 @@ test("Forbid to manage by default", t => {
   const acl = getAbilities(new User({}))
 
   t.true(acl.cannot("manage", new User({})))
+})
+
+test("Allow moderator to update user", t => {
+  const acl = getAbilities(new User({role: roles.moderator}))
+
+  t.true(acl.can("update", new User({})))
+})
+
+test("Restrict moderator on updating specific user's fields", t => {
+  const acl = getAbilities(new User({role: roles.moderator}))
+
+  const actual = permittedFieldsOf(acl, "update", new User({}))
+
+  t.deepEqual(actual, ["status"])
+})
+
+test("Forbid moderator to update moderator", t => {
+  const acl = getAbilities(new User({role: roles.moderator}))
+
+  t.true(acl.cannot("update", new User({role: roles.moderator})))
+})
+
+test("Forbid moderator to update admin", t => {
+  const acl = getAbilities(new User({role: roles.moderator}))
+
+  t.true(acl.cannot("update", new User({role: roles.admin})))
+})
+
+test("Forbid moderator to update super", t => {
+  const acl = getAbilities(new User({role: roles.moderator}))
+
+  t.true(acl.cannot("update", new User({role: roles.super})))
+})
+
+test("Forbid moderator to update support", t => {
+  const acl = getAbilities(new User({role: roles.moderator}))
+
+  t.true(acl.cannot("update", new User({role: roles.support})))
+})
+
+test("Forbid moderator to update tech", t => {
+  const acl = getAbilities(new User({role: roles.moderator}))
+
+  t.true(acl.cannot("update", new User({role: roles.tech})))
+})
+
+test("Forbid moderator to update developer", t => {
+  const acl = getAbilities(new User({role: roles.moderator}))
+
+  t.true(acl.cannot("update", new User({role: roles.developer})))
+})
+
+test("Allow admin to update user", t => {
+  const acl = getAbilities(new User({role: roles.admin}))
+
+  t.true(acl.can("update", new User({})))
+})
+
+test("Allow admin to update moderator", t => {
+  const acl = getAbilities(new User({role: roles.admin}))
+
+  t.true(acl.can("update", new User({role: roles.moderator})))
+})
+
+test("Allow admin to update tech", t => {
+  const acl = getAbilities(new User({role: roles.admin}))
+
+  t.true(acl.can("update", new User({role: roles.tech})))
+})
+
+test("Allow admin to update support", t => {
+  const acl = getAbilities(new User({role: roles.admin}))
+
+  t.true(acl.can("update", new User({role: roles.support})))
 })
 
 test("Allow super to manage users", t => {
