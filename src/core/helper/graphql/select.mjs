3b6cb1361ae7a2bce5d907cd41fef5f1645a0b51
Nick K <nick.kruchinin@gmail.com>
2019-08-03 04:21:33 +0300

    Improve core/helper/graphql/select function.

--- src/core/helper/graphql/select.mjs
+++ src/core/helper/graphql/select.mjs
@@ -3,14 +3,26 @@ import {fieldsList} from "graphql-fields-list"
 import isEmpty from "lodash/isEmpty"
 import mongoose from "mongoose"
 
+import flat from "core/helper/array/flat"
+
 const {Query} = mongoose
 
-const select = node => query => {
+/**
+ * Calls .select method on given query using requested fields from
+ * the current GraphQL operation. Optionally can apply additional fields.
+ *
+ * @param {object} node
+ * @param {string[]} fields
+ *
+ * @return {Function} – A function that takes the query and applies fields to
+ * its .select() method
+ */
+const select = (node, ...fields) => query => {
   if (!(query instanceof Query) || isEmpty(node)) {
     return query
   }
 
-  return query.select(fieldsList(node))
+  return query.select(fieldsList(node).concat(flat(fields)))
 }
 
 export default select
