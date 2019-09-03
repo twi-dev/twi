833745072efa02e72a6c23d9924fdce71c04e6da
Nick K <nick.kruchinin@gmail.com>
2019-09-03 22:28:31 +0300

    Small fix.

--- src/model/StoryChapters/StoryChapters.mjs
+++ src/model/StoryChapters/StoryChapters.mjs
@@ -5,7 +5,7 @@ import createModel from "core/db/createModel"
 import indexes from "./indexes"
 import schema from "./schema"
 
-@createModel(schema, {indexes})
+@createModel(schema, {indexes, timestamps: false})
 class StoryChapters extends Model {
   static tableName = "story_chapters"
 }
