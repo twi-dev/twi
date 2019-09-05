f826d2666458a1837e4b53182ab229ee1f9a93d2
Nick K <nick.kruchinin@gmail.com>
2019-09-06 00:46:07 +0300

    Improve authRefreshTokens resolver.

--- src/api/resolve/mutation/auth/refreshTokens.mjs
+++ src/api/resolve/mutation/auth/refreshTokens.mjs
@@ -4,10 +4,10 @@ import Session from "model/Session"
 
 async function refreshTokens({args, ctx}) {
   const session = await Session.findByToken(args.refreshToken)
-  const user = await session.getUser()
+  const user = await session.getUser({attributes: {exclude: ["password"]}})
   const tokens = await session.refresh({user, client: ctx.state.client})
 
-  return user.update({$set: {"dates.lastVisit": tokens.accessToken.signed}})
+  return user.update({lastVisit: tokens.accessToken.signed})
     .then(() => tokens)
 }
 
