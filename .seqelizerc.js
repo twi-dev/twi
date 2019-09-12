254d879425d16117b98ddc4c032232c667a0f6e6
Nick K <nick.kruchinin@gmail.com>
2019-09-13 01:27:38 +0300

    Add .sequelizerc.js draft.

---
+++ .seqelizerc.js
@@ -0,0 +1,9 @@
+const {join} = require("path")
+
+module.exports = {
+  // TODO: Add a separate database config loader to setup
+  // config: join(__dirname, "core", "base", "config"),
+  "models-path": join(__dirname, "model"),
+  "seeders-path": join(__dirname, "setup", "db", "seeder"),
+  "migrations-path": join(__dirname, "setup", "db", "migration")
+}
