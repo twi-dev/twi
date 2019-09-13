d85089c097ff1deb3c4bdff3966544952eaae326
Nick K <nick.kruchinin@gmail.com>
2019-09-14 00:30:40 +0300

    Fix for dates serialization.

--- src/core/helper/graphql/serializeDate.mjs
+++ src/core/helper/graphql/serializeDate.mjs
@@ -1,9 +1,10 @@
+import parseISO from "date-fns/parseISO"
 import toDate from "date-fns/toDate"
 
 const serializeDate = name => parent => {
   const field = parent[name]
 
-  return field == null ? field : toDate(field)
+  return field == null ? field : field |> parseISO |> toDate
 }
 
 export default serializeDate
