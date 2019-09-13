c869f0bf8c1b8867ade6ff892f0303e0dfbcb1e4
Nick K <nick.kruchinin@gmail.com>
2019-09-13 04:15:45 +0300

    User INNER JOIN in chapter query instead of LEFT OUTER JOIN.

--- src/api/resolve/query/story/chapter.mjs
+++ src/api/resolve/query/story/chapter.mjs
@@ -6,16 +6,15 @@ import Chapter from "model/Chapter"
 
 import getStoryAbilities from "acl/story"
 
+const include = [{model: Story, as: "story", required: true}]
+
 async function getChapter({args, ctx}) {
   const {storyId, chapterNumber: order} = args
   const {user} = ctx.state
 
   const acl = getStoryAbilities({user})
 
-  const chapter = await Chapter.findOne({
-    where: {storyId, order},
-    include: [{model: Story, as: "story"}]
-  })
+  const chapter = await Chapter.findOne({include, where: {storyId, order}})
 
   if (!chapter || acl.cannot("read", chapter.story)) {
     throw new NotFound("Can't finnd requested chapter.")
