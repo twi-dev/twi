aa2b9a5c687a771346e3fa45eedfeb7e4027ea9a
Nick K <nick.kruchinin@gmail.com>
2019-09-17 03:16:14 +0300

    Fix for avatar remove.

--- src/api/resolve/mutation/user/avatarRemove.mjs
+++ src/api/resolve/mutation/user/avatarRemove.mjs
@@ -1,19 +1,30 @@
+import waterfall from "core/helper/array/runWaterfall"
 import bind from "core/helper/graphql/normalizeParams"
 import NotFound from "core/error/http/NotFound"
 import auth from "core/auth/checkUser"
+import conn from "core/db/connection"
 
 import User from "model/User"
+import File from "model/File"
 
-async function avatarRemove({ctx}) {
-  const {user} = ctx.state
+const include = [{model: File, as: "avatar"}]
 
-  const found = await User.findByPk(user.id)
+const avatarRemove = ({ctx}) => conn.transaction(async transaction => {
+  const {user: viewer} = ctx.state
+
+  const user = await User.findByPk(viewer.id, {include, transaction})
 
   if (!user) {
     throw new NotFound("Can't find requested user.")
   }
 
-  return found.avatarRemove().then(() => found.reload())
-}
+  return waterfall([
+    () => user.avatar.destroy({transaction}),
+
+    () => user.avatarRemove({transaction}),
+
+    () => user.avatar.id
+  ])
+})
 
 export default avatarRemove |> auth |> bind
