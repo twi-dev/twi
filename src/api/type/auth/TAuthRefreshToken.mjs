3d6afc389eb486ac22a7581b49f644f2cdb9e152
Nick K <nick.kruchinin@gmail.com>
2019-09-19 03:57:54 +0300

    Fix AuthRefreshToken type description.

--- src/api/type/auth/TAuthRefreshToken.mjs
+++ src/api/type/auth/TAuthRefreshToken.mjs
@@ -4,7 +4,8 @@ import TAuthTokenMinimal from "./TAuthTokenMinimal"
 
 const TAuthRefreshToken = Type({
   name: "AuthRefreshToken",
-  description: "An access token with it expiration date",
+  description: "A long-term token used to refresh authorization "
+    + "tokens pair within the current session.",
   extends: TAuthTokenMinimal
 }).end()
 
