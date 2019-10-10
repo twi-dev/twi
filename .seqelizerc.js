a60e34bdfaba730e4387929569a2bbfe9bd4eaca
Nick K <nick.kruchinin@gmail.com>
2019-10-11 01:59:20 +0300

    Small fixes.

--- .seqelizerc.js
+++ .seqelizerc.js
@@ -2,7 +2,7 @@ const {join} = require("path")
 
 module.exports = {
   // TODO: Add a separate database config loader to setup
-  // config: join(__dirname, "core", "base", "config"),
+  // config: join(__dirname, "setup", "config"),
   "models-path": join(__dirname, "model"),
   "seeders-path": join(__dirname, "setup", "db", "seeder"),
   "migrations-path": join(__dirname, "setup", "db", "migration")
