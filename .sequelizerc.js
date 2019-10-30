7cfb1728e31b3f73985d2a02178a9d3c4df1d18e
Nick K <nick.kruchinin@gmail.com>
2019-10-30 22:31:28 +0300

    Fix for setup db config.

--- .sequelizerc.js
+++ .sequelizerc.js
@@ -1,7 +1,7 @@
 const {join} = require("path")
 
 module.exports = {
-  config: join(__dirname, "setup", "config.js"),
+  config: join(__dirname, "setup", "db", "config.js"),
   "models-path": join(__dirname, "model"),
   "seeders-path": join(__dirname, "setup", "db", "seed"),
   "migrations-path": join(__dirname, "setup", "db", "migration")
