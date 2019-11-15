189ac928cce8c9790ed7dc8f62b9f4922a0705f0
Nick K <nick.kruchinin@gmail.com>
2019-11-15 11:18:33 +0300

    Improve migrations.

--- src/setup/db/seed/20191012091039-categories-and-tags.mjs
+++ src/setup/db/seed/20191012091039-categories-and-tags.mjs
@@ -2,12 +2,13 @@ import {join} from "path"
 
 import {readFile} from "promise-fs"
 
+import last from "lodash/last"
 import first from "lodash/first"
 import isEmpty from "lodash/isEmpty"
 
 import createSlug from "core/helper/util/createSlug"
 
-import categoriesData from "setup/db/data/categories.json"
+import data from "setup/db/data/categories.json"
 
 async function loadTags(name) {
   let tags = []
@@ -28,31 +29,60 @@ async function loadTags(name) {
 }
 
 const up = q => q.sequelize.transaction(async transaction => {
-  let categories = await q.bulkInsert(
-    "categories",
-
-    categoriesData.map(
-      (name, order) => ({
-        name,
-        slug: createSlug(name),
-        order: order + 1,
-        created_at: new Date(),
-        updated_at: new Date()
-      })
-    ),
+  let categories = await q.sequelize.query(
+    "SELECT * FROM categories WHERE slug IN (:categories)",
 
     {
-      transaction
-    }
-  )
+      transaction,
 
-  categories = await q.sequelize.query(
-    "SELECT * FROM categories",
-
-    {
-      transaction
+      raw: true,
+      replacements: {
+        categories: data.map(name => createSlug(name))
+      }
     }
   ).then(first)
+
+  let fields = data
+  if (!isEmpty(categories)) {
+    fields = data
+      .filter(name => !categories.some(category => category.name === name))
+  }
+
+  if (!isEmpty(fields)) {
+    categories = await q.bulkInsert(
+      "categories",
+
+      fields
+        .map(
+          (name, order) => ({
+            name,
+            slug: createSlug(name),
+            order: isEmpty(categories)
+              ? order + 1
+              : last(categories).order + order + 1,
+            created_at: new Date(),
+            updated_at: new Date()
+          })
+        ),
+
+      {
+        transaction
+      }
+    )
+
+    categories = await q.sequelize.query(
+      "SELECT * FROM categories WHERE name IN (:categories)",
+
+      {
+        transaction,
+
+        raw: true,
+        replacements: {
+          categories: fields
+        }
+      }
+    ).then(first)
+  }
 
   categories = await Promise.all(
     categories.map(
@@ -60,6 +90,7 @@ const up = q => q.sequelize.transaction(async transaction => {
     )
   )
 
+  // FIXME: Dedupe tags
   await Promise.all(
     categories
       .filter(({tags}) => !isEmpty(tags))
