47486e3c11dc24cd73529713a90a4f9cf7ad0819
Nick K <nick.kruchinin@gmail.com>
2019-11-25 23:20:48 +0300

    Add hooks for category ordering updates.

--- src/model/Category/Category.mjs
+++ src/model/Category/Category.mjs
@@ -1,4 +1,4 @@
-import {Model} from "sequelize"
+import {Model, Op as op} from "sequelize"
 
 import createModel from "core/db/createModel"
 
@@ -6,5 +6,23 @@ import schema from "./schema"
 
 @createModel(schema)
 class Category extends Model { }
+
+Category.beforeCreate(async (category, options) => {
+  const count = await Category.count(options)
+
+  category.order = count + 1
+})
+
+Category.afterDestroy(({id}, options) => Category.decrement(
+  "order",
+
+  {
+    ...options,
+
+    where: {
+      [op.gt]: id
+    }
+  }
+))
 
 export default Category
