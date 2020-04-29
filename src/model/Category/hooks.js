31f8d2e7399ffef26379733432b429d0bfcfad3d
octet-stream <nick.kruchinin@gmail.com>
2020-04-30 00:00:27 +0300

    Small fixes.

--- src/model/Category/hooks.js
+++ src/model/Category/hooks.js
@@ -1,12 +1,28 @@
 import {Op as op} from "sequelize"
 
+/**
+ * @typedef {import("./Category").default} Category
+ */
+
+/**
+ * @param {Category} Category
+ */
 const hooks = Category => ({
+  /**
+   *
+   * @param {Category} category
+   * @param {Object<string, any>} [options]
+   */
   async beforeCreate(category, options) {
     const count = await Category.count(options)
 
     category.order = count + 1
   },
 
+  /**
+   * @param {Object} category
+   * @param {number} category.id
+   */
   afterDestroy: (({id}, options) => Category.decrement("order", {
     ...options,
 
