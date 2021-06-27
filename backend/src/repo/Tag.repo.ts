d826cb7396eb7b9ac031c81653b918e132fd1f2f
Nick K <nick.kruchinin@gmail.com>
2021-06-27 19:14:41 +0300

    backend: Make tags filtering case-insensitive

--- backend/src/repo/Tag.repo.ts
+++ backend/src/repo/Tag.repo.ts
@@ -10,10 +10,10 @@ export class TagRepo extends Repository<Tag> {
   async findOrCreateMany(tags: string[]): Promise<Tag[]> {
     const found = await this.find({where: {name: In(tags)}, select: ["name"]})
 
-    const names = found.map(({name}) => name)
+    const names = found.map(({name}) => name.toLowerCase())
 
     const created = tags
-      .filter(name => !names.includes(name))
+      .filter(name => !names.includes(name.toLowerCase()))
       .map(name => this.create({name}))
 
     if (!isEmpty(created)) {
