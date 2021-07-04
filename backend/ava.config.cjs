d72e2afd53102f27722316bffaafd685e0ce9b44
Nick K <nick.kruchinin@gmail.com>
2021-07-04 04:06:45 +0300

    Add ava.config.cjs

---
+++ backend/ava.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  extensions: [
+    "ts"
+  ],
+  files: ["**/*.test.ts"],
+  require: [
+    "ts-node/register/transpile-only",
+    "reflect-metadata"
+    "./src/config.ts"
+  ]
+}
