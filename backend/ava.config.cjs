0b658589eede620d7bf40a772d1670a858c90418
Nick K <nick.kruchinin@gmail.com>
2021-07-08 21:14:04 +0300

    backend: Add tests for storyChapterUpdate mutation.

--- backend/ava.config.cjs
+++ backend/ava.config.cjs
@@ -1,4 +1,8 @@
 module.exports = {
+  // * This param was increased due to issues with test,
+  // * likely caused by slow TS compilation with ts-node.
+  // * I need to find a way to increase compilation speed by keep current path resolving strategy
+  timeout: "2m",
   failFast: true,
   environmentVariables: {
     NODE_ENV: "test",
--- backend/src/api/resolver/ChapterResolver.mutation.test.ts
+++ backend/src/api/resolver/ChapterResolver.mutation.test.ts
@@ -14,6 +14,7 @@ import {User} from "entity/User"
 
 import schema from "api/schema"
 import StoryChapterAddInput from "api/input/story/ChapterAdd"
+import ChapterUpdateInput from "api/input/chapter/Update"
 
 import createFakeChapters from "__helper__/createFakeChapters"
 import createFakeStories from "__helper__/createFakeStories"
@@ -30,6 +31,17 @@ const test = ava as TestInterface<{
 const storyChapterAdd = /* GraphQL */ `
   mutation StoryChapterAdd($story: StoryChapterAddInput!) {
     storyChapterAdd(story: $story) {
+      id
+      title
+      description
+      text
+    }
+  }
+`
+
+const storyChapterUpdate = /* GraphQL */ `
+  mutation StoryChapterUpdate($chapter: ChapterUpdateInput!) {
+    storyChapterUpdate(chapter: $chapter) {
       id
       title
       description
@@ -87,6 +99,78 @@ test("storyChapterAdd creates a new chapter", async t => {
   t.is(data!.storyChapterAdd.title, chapter!.title)
   t.is(data!.storyChapterAdd.description, chapter!.description)
   t.is(data!.storyChapterAdd.text, chapter!.text)
+})
+
+test("storyChapterUpdate updates a title", async t => {
+  const {user, story, db} = t.context
+
+  const [{title}, chapter] = createFakeChapters(2)
+
+  chapter.story = story
+
+  await db.getCustomRepository(ChapterRepo).save(chapter)
+
+  const {data, errors} = await graphql({
+    schema,
+    source: storyChapterUpdate,
+    contextValue: createFakeContext({session: {userId: user.id}}),
+    variableValues: {
+      chapter: {id: chapter.id, title} as ChapterUpdateInput
+    }
+  })
+
+  t.falsy(errors)
+
+  t.not(data!.storyChapterUpdate.title, chapter.title)
+  t.is(data!.storyChapterUpdate.title, title)
+})
+
+test("storyChapterUpdate updates a description", async t => {
+  const {user, story, db} = t.context
+
+  const [{description}, chapter] = createFakeChapters(2)
+
+  chapter.story = story
+
+  await db.getCustomRepository(ChapterRepo).save(chapter)
+
+  const {data, errors} = await graphql({
+    schema,
+    source: storyChapterUpdate,
+    contextValue: createFakeContext({session: {userId: user.id}}),
+    variableValues: {
+      chapter: {id: chapter.id, description} as ChapterUpdateInput
+    }
+  })
+
+  t.falsy(errors)
+
+  t.not(data!.storyChapterUpdate.description, chapter.description)
+  t.is(data!.storyChapterUpdate.description, description)
+})
+
+test("storyChapterUpdate updates a text", async (t) => {
+  const {user, story, db} = t.context
+
+  const [{text}, chapter] = createFakeChapters(2)
+
+  chapter.story = story
+
+  await db.getCustomRepository(ChapterRepo).save(chapter)
+
+  const {data, errors} = await graphql({
+    schema,
+    source: storyChapterUpdate,
+    contextValue: createFakeContext({session: {userId: user.id}}),
+    variableValues: {
+      chapter: {id: chapter.id, text} as ChapterUpdateInput
+    }
+  })
+
+  t.falsy(errors)
+
+  t.not(data!.storyChapterUpdate.text, chapter.text)
+  t.is(data!.storyChapterUpdate.text, text)
 })
 
 test.after(async () => {
