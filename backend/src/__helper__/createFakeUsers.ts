f0c74cd8b0b9c2a68b5e6cc8a545c32041ab816a
Nick K <nick.kruchinin@gmail.com>
2021-07-09 04:02:27 +0300

    Small fixes

--- backend/src/__helper__/createFakeUsers.ts
+++ backend/src/__helper__/createFakeUsers.ts
@@ -12,8 +12,11 @@ const createFakeUsers = (
   amount,
 
   user => {
-    user.login = faker.internet.userName()
-    user.email = faker.internet.email()
+    const firstName = faker.name.findName()
+    const lastName = faker.name.lastName()
+
+    user.login = faker.internet.userName(firstName, lastName)
+    user.email = faker.internet.email(firstName, lastName)
     user.password = faker.internet.password()
     user.status = UserStatuses.ACTIVE
     user.role = UserRoles.REGULAR
