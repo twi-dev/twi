28943a2c1aaba035a2a1184e0c519f7c87fa7c16
Nick K <nick.kruchinin@gmail.com>
2021-07-09 03:31:14 +0300

    Speed up tests compilation using ts-node/register/transpile-only + tsconfig-paths/register instead of ttypescript. It also affects development environment.

--- backend/ava.config.cjs
+++ backend/ava.config.cjs
@@ -1,13 +1,7 @@
 module.exports = {
-  // * This param was increased due to issues with test,
-  // * likely caused by slow TS compilation with ts-node.
-  // * I need to find a way to increase compilation speed by keep current path resolving strategy
-  timeout: "2m",
   failFast: true,
   environmentVariables: {
     NODE_ENV: "test",
-    TS_NODE_COMPILER: "ttypescript",
-    TS_NODE_PROJECT: "tsconfig.json",
     TS_NODE_PREFER_TS_EXTS: "true"
   },
   extensions: [
@@ -15,7 +9,7 @@ module.exports = {
   ],
   files: ["**/*.test.ts"],
   require: [
-    "ts-node/register",
+    "ts-node/register/transpile-only",
     "reflect-metadata",
     "./src/config.ts"
   ]
