2462918de9e71b0617539af6920b5e210cd23aa5
Nick K <nick.kruchinin@gmail.com>
2021-07-24 23:09:29 +0300

    backend: Add restore and restoreAndFlush methods to BaseEntity.

--- backend/src/repo/BaseRepo.ts
+++ backend/src/repo/BaseRepo.ts
@@ -1,6 +1,20 @@
 import {EntityRepository, EntityManager} from "@mikro-orm/mysql"
+import {EntityMetadata} from "@mikro-orm/core"
 
 import {BaseEntitySoftRemovable} from "entity/BaseEntitySoftRemovable"
+
+function assertColumn(
+  meta: EntityMetadata,
+  name: string,
+  action: string
+): void {
+  if (!("deletedAt" in meta.properties)) {
+    throw Error(
+      `Cannot perform ${action} action on ${name} entity. `
+        + "The deletedAt column is missing in its properties list."
+    )
+  }
+}
 
 export abstract class BaseRepo<
   T extends BaseEntitySoftRemovable
@@ -13,12 +27,7 @@ export abstract class BaseRepo<
     const storage = this.em.getMetadata()
     const meta = storage.get(String(this.entityName))
 
-    if (!("deletedAt" in meta.properties)) {
-      throw Error(
-        `Cannot perform softRemove action on ${this.entityName} entity. `
-          + "The deletedAt column is missing in its properties list."
-      )
-    }
+    assertColumn(meta, String(this.entityName), "softRemove")
 
     // Do nothing and return EntityManager if entity deletedAt column has a date.
     if (entity.deletedAt !== null) {
@@ -36,5 +45,26 @@ export abstract class BaseRepo<
    */
   softRemoveAndFlush(entity: T): Promise<void> {
     return this.softRemove(entity).flush()
+  }
+
+  /**
+   * Marks soft removed entity for restoring.
+   * This method will remove a value from deletedAt column.
+   */
+  restore(entity: T): EntityManager {
+    const storage = this.em.getMetadata()
+    const meta = storage.get(String(this.entityName))
+
+    assertColumn(meta, String(this.entityName), "restore")
+
+    return this.em.persist(entity)
+  }
+
+  /**
+   * Marks soft removed entity for restoring and persists it immediately.
+   * Equivalent to `em.restore(e).flush()`
+   */
+  restoreAndFlush(entity: T): Promise<void> {
+    return this.restore(entity).flush()
   }
 }
