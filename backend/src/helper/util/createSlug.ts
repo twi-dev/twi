b4e81401a3b5a2c463897980f0a6d4da4ecc9a00
Nick K <nick.kruchinin@gmail.com>
2022-05-13 18:46:03 +0300

    Improve createSlug helper

--- backend/src/helper/util/createSlug.ts
+++ backend/src/helper/util/createSlug.ts
@@ -1,15 +1,15 @@
 import create from "@sindresorhus/slugify"
 
+const customReplacements: readonly [string, string][] = [
+  [":", "colon"],
+  [",", "comma"],
+  [".", "period"],
+  ["@", "at"]
+].map(([symbol, replacement]) => [symbol, ` ${replacement} `])
+
 /**
  * @param string a value to slugify
  */
-const createSlug = (string: string) => create(string, {
-  customReplacements: [
-    [":", " colon "],
-    [",", " comma "],
-    [".", " period "],
-    ["@", " at "]
-  ]
-})
+const createSlug = (string: string) => create(string, {customReplacements})
 
 export default createSlug
