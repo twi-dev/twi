3813863f594c044d9c1518fd0d0b7aa6760a94b2
Nick K <nick.kruchinin@gmail.com>
2023-08-26 00:16:56 +0300

    Fix for serverAddress utility

--- lib/utils/serverAddress.ts
+++ lib/utils/serverAddress.ts
@@ -2,7 +2,11 @@ import {z} from "zod"
 
 import {globalObject} from "./globalObject"
 
-const ServerAddress = z.string().url().transform(url => new URL(url).origin)
+const ServerAddress = z
+  .string()
+  .url()
+  .default("http://localhost:3000") // FIXME: `nuxt prepare` if I remove this. Find proper fix
+  .transform(url => new URL(url).origin)
 
 export const serverAddress = ServerAddress.parse(
   process.env.AUTH_ORIGIN || globalObject?.location?.origin
