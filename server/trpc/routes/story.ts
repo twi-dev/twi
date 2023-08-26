890801667b188a7460f6ba40388b936378bd593f
Nick K <nick.kruchinin@gmail.com>
2023-08-26 03:42:17 +0300

    fix(api,story): Expose story.create procedure to tRPC router.

--- server/trpc/routes/story.ts
+++ server/trpc/routes/story.ts
@@ -1,7 +1,9 @@
 import {router} from "../def.js"
 
 import {getBySlug} from "./story/getBySlug.js"
+import {create} from "./story/create.js"
 
 export const story = router({
-  getBySlug
+  getBySlug,
+  create
 })
