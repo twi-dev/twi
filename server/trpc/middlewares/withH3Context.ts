e2b83060c723b1d72a9cda8d64f6c1d4d7dbd246
Nick K <nick.kruchinin@gmail.com>
2023-08-26 04:33:19 +0300

    fix(api): Fix a typo in withH3Context middleware

--- server/trpc/middlewares/withH3Context.ts
+++ server/trpc/middlewares/withH3Context.ts
@@ -7,7 +7,7 @@ export const withH3Context = middleware(({ctx, next}) => {
   if (!isH3Context(ctx)) {
     throw new TRPCError({
       code: "INTERNAL_SERVER_ERROR",
-      message: "FetchContext required for this operation"
+      message: "H3Context required for this operation"
     })
   }
 
