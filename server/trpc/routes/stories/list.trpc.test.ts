46ca10a596ab81524f2d665d9cb0b7a944cf5604
Nick K <nick.kruchinin@gmail.com>
2023-08-27 18:03:06 +0300

    tests(api,stories): Fix for a test

--- server/trpc/routes/stories/list.trpc.test.ts
+++ server/trpc/routes/stories/list.trpc.test.ts
@@ -1,7 +1,10 @@
 import {describe, beforeAll, afterAll} from "vitest"
 import {faker} from "@faker-js/faker"
+import {z} from "zod"
 
 import {trpcTest} from "../../../../scripts/tests/context/trpc.js"
+import {StoryBaseOutput} from "../../types/story/StoryBaseOutput.js"
+import {UserStatuses} from "../../types/user/UserStatuses.js"
 import {User, Story} from "../../../db/entities.js"
 import {getORM} from "../../../lib/db/orm.js"
 
@@ -15,7 +18,8 @@ describe("stories.list procedure", async () => {
     const user = em.create(User, {
       login: faker.internet.userName(),
       email: faker.internet.email(),
-      password: faker.internet.password()
+      password: faker.internet.password(),
+      status: UserStatuses.Active
     })
 
     const createStories = () => em.create(Story, {
@@ -41,9 +45,13 @@ describe("stories.list procedure", async () => {
 
   trpcTest("returns a list when called w/o arguments", async ({
     trpc,
-    expect
+    expect,
+    orm
   }) => {
     const page = await trpc.stories.list()
+
+    const stories = await orm.em.find(Story, {}, {orderBy: {createdAt: "desc"}})
+    const expected = await z.array(StoryBaseOutput).parseAsync(stories)
 
     expect(page.current).to.equal(1)
     expect(page.hasItems).to.equal(true)
@@ -54,6 +62,7 @@ describe("stories.list procedure", async () => {
     expect(page.rowsCount).to.equal(count)
     expect(page.prevCursor).to.toBeNull()
     expect(page.nextCursor).to.toBeNull()
+    expect(page.items).to.be.deep.equal(expected)
   })
 
   trpcTest("has next page", async ({trpc, expect}) => {
