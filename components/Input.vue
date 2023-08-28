b173a6043a18140febd80c462079cfe7751bb8ca
Nick K <nick.kruchinin@gmail.com>
2023-08-29 02:34:58 +0300

    fix: Fix two-way binding for Input component

--- components/Input.vue
+++ components/Input.vue
@@ -1,19 +1,24 @@
 <script setup lang="ts">
 interface Props {
   wide?: boolean
+  modelValue?: string
 }
 
 defineProps<Props>()
+defineEmits(["update:modelValue"])
 </script>
 
 <template>
   <input
-  :class='[
+    :class='[
       "p-3 border border-gray-300 text-neutral-500 dark:text-neutral-400 rounded-md dark:bg-neutral-900 dark:border-neutral-600",
 
       {
         "w-full": wide === true
       },
     ]'
+
+    :value="modelValue"
+    @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)"
   />
 </template>
