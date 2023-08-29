5c1b7258a4403c2feb6674ead9bdf2f67584b215
Nick K <nick.kruchinin@gmail.com>
2023-08-29 04:34:19 +0300

    feat(nav): Add minimal header navigation and Delimiter component

---
+++ components/Delimiter.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+interface Props {
+  orientation?: "vertical" | "horizontal"
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <div
+    :class='[
+      "bg-neutral-200 dark:bg-neutral-800",
+
+      {
+        "w-px h-full": orientation === "vertical",
+        "w-full h-px": orientation === "horizontal"
+      }
+    ]'
+  />
+</template>
--- components/Header.vue
+++ components/Header.vue
@@ -1,7 +1,29 @@
+<script setup lang="ts">
+import {LogOut, User2} from "lucide-vue-next"
+import {isEmpty} from "lodash-es"
+
+const {getSession} = useAuth()
+
+const session = await getSession()
+</script>
+
 <template>
   <header class="p-5 desktop:px-0 bg-transparent border-b border-neutral-200 dark:border-neutral-800">
-    <DesktopContainer>
-      Twilight's Library
+    <DesktopContainer class="flex flex-row">
+      <div role="logo">
+        Twilight's Library
+      </div>
+
+      <div class="flex-1" />
+
+      <nav>
+        <NuxtLink v-if="isEmpty(session)" href="/auth/login">
+          <User2 />
+        </NuxtLink>
+
+        <LogOut v-else />
+      </nav>
+
     </DesktopContainer>
   </header>
 </template>
