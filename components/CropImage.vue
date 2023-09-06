d6f4956158423573c5cf2432efe3a831e625e46f
Nick K <nick.kruchinin@gmail.com>
2023-09-06 16:20:25 +0300

    feat(crop): Add ImageCrop component

---
+++ components/CropImage.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import Cropper from "cropperjs"
+
+export interface CropImageProps {
+  src: string
+  alt?: string
+}
+
+defineProps<CropImageProps>()
+
+const cropper = ref<Cropper>()
+const el = ref<HTMLImageElement>()
+
+onMounted(() => {
+  if (el.value) {
+    cropper.value = new Cropper(el.value, {
+      aspectRatio: 1
+    })
+  }
+})
+
+onUnmounted(() => {
+  if (cropper.value) {
+    cropper.value.clear()
+  }
+})
+</script>
+
+<template>
+  <div>
+    <img ref="el" :src="src" :alt="alt" />
+  </div>
+</template>
