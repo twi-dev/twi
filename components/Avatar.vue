04ee51425b63a8b997b66d58bb7e94f4003d619c
Nick K <nick.kruchinin@gmail.com>
2023-09-23 02:23:25 +0300

    fix(avatar): Render user avatar with nuxt/image

--- components/Avatar.vue
+++ components/Avatar.vue
@@ -8,10 +8,26 @@ export interface AvatarProps {
   src?: string
 }
 
-withDefaults(defineProps<AvatarProps>(), {
+// TODO: Use common type
+interface ImageSize {
+  width: number
+  height: number
+}
+
+const toSquareSize = (size: number): ImageSize => ({width: size, height: size})
+
+const sizes = Object.freeze<Readonly<Record<AvatarSizes, ImageSize>>>({
+  sm: toSquareSize(32),
+  md: toSquareSize(80),
+  lg: toSquareSize(128)
+})
+
+const {size} = withDefaults(defineProps<AvatarProps>(), {
   size: "sm",
   src: undefined
 })
+
+const {width, height} = sizes[size] || sizes.sm
 </script>
 
 <template>
@@ -20,12 +36,12 @@ withDefaults(defineProps<AvatarProps>(), {
       {
         'w-8 h-8': size === 'sm',
         'w-20 h-20': size === 'md',
-        'w-32': size === 'lg'
+        'w-32 h-32': size === 'lg'
       }
     ]"
   >
     <div class="w-full h-full rounded-full border-2 border-black dark:border-white flex items-center justify-center overflow-hidden">
-      <img v-if="src" :src="src" alt="User avatar" />
+      <NuxtPicture v-if="src" :src="src" :width="width" :height="height" alt="My avatar" />
 
       <User2 v-else class="text-black-200 dark:text-white w-full h-full" />
     </div>
