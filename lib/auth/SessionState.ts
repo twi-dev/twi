2bb2185136a4964763da01742f8ee945de3f45a0
Nick K <nick.kruchinin@gmail.com>
2023-10-22 02:14:46 +0300

    chore(auth): Remove unused SessionState type

--- lib/auth/SessionState.ts
+++
@@ -1,8 +0,0 @@
-import {Session} from "next-auth"
-
-import type {Maybe} from "../utils/types/Maybe.js"
-
-export interface SessionState {
-  session: Ref<Maybe<Session>>
-  refresh(): Promise<void>
-}
