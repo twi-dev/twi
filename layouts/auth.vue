d043299c259a73031c56aad047a60f02418c8d49
Nick K <nick.kruchinin@gmail.com>
2023-12-22 17:16:26 +0300

    chore(layouts): Remove unnecessary import from auth layout and rely on auto-import

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -1,7 +1,3 @@
-<script setup lang="ts">
-import FullscreenMain from "../components/FullscreenMain.vue"
-</script>
-
 <template>
   <FullscreenMain>
     <div class="w-full m-auto p-5 mobile:w-mobile">
